948cf2b6f55ef9d60ff5647a454a637ddb22d609
G4vp <gviera2442@gmail.com>
2025-09-13 19:08:55 -0700

    Add Resume in Resume Page

--- g4vp/src/pages/ResumePage.tsx
+++ g4vp/src/pages/ResumePage.tsx
@@ -10,6 +10,10 @@ function ResumePage() {
       <NoiseFilter/>
       <HeaderBlock title="Resume" subtitle="Last update of resume: Sept 25, 2025"/>
       <div className="card-content">
+        <div className="resume-sub-content">
+          <object data="/Gabriel_Viera_Resume.pdf" type="application/pdf" width="100%" height="100%">
+          </object> 
+        </div>
         <NavBar/>
         <Footer/>
       </div>
